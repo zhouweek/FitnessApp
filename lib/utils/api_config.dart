6cfa915ef810f95c7e4e3cb8eaa469635e61a8b5
taodongzhou.ijei <taodongzhou.ijei@bytedance.com>
2026-04-14 21:25:11 +0800

    修改app host

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,10 +1,10 @@
 class ApiConfig {
-  static const String host = '10.82.70.93';
-  static const int port = 8000;
-  static const String protocol = 'http';
+  static const String host = 'fitnessapp-rxlv.onrender.com';
+  static const int port = 443;
+  static const String protocol = 'https';
   static const String apiPrefix = '/api/v1';
   
-  static const String baseUrl = '$protocol://$host:$port$apiPrefix';
+  static const String baseUrl = '$protocol://$host$apiPrefix';
   
   static const String authRegister = '/auth/register';
   static const String authLogin = '/auth/login';
